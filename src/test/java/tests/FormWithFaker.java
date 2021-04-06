e02e60f21364a3c348a9c59f9d44848304c636dc
janemiro <jane.miroshina@gmail.com>
2021-04-06 21:49:25 +0300

    Update FormWithFaker.java

--- src/test/java/tests/FormWithFaker.java
+++ src/test/java/tests/FormWithFaker.java
@@ -36,59 +36,38 @@ public class FormWithFaker {
     @Test
     public void FormTest() {
         open("https://demoqa.com/automation-practice-form");
-
-        // имя, мейл
+        
         $("#firstName").val(firstName);
         $("#lastName").val(lastName);
         $("#userEmail").val(userEmail);
-
-        // пол
         $(byText(gender)).click();
-
-        // мобильный
         $("#userNumber").val(userNumber);
-
-        // календарь
+        
         $("#dateOfBirthInput").click();
-
         $(".react-datepicker__month-select").selectOption(monthOfBirth);
         $(".react-datepicker__year-select").selectOption(yearOfBirth);
         $$(".react-datepicker__day--0" + dayOfBirth)
                 .filter(not(cssClass("react-datepicker__day--outside-month")))
                 .first().click();
-
-        // предметы
+        
         $("#subjectsInput").val(subject1).pressEnter();
         $("#subjectsInput").val(subject2);
         $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
-
-        // хобби
         $("#hobbiesWrapper").$(byText(hobby1)).click();
         $("#hobbiesWrapper").$(byText(hobby2)).click();
-
-        // загрузить картинку
         $("#uploadPicture").uploadFromClasspath(picture);
 
-        // адрес
         $("#currentAddress").val(currentAddress);
         $("#state").scrollTo().click();
         $("#stateCity-wrapper").$(byText(state)).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText(city)).click();
 
-        // сабмит
         $("#submit").click();
 
-        // посмотреть результат
-        Selenide.sleep(3000);
-
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text(firstName + " " + lastName),
-                text(userEmail), text(gender));
+        $(".table-responsive").shouldHave(text(firstName + " " + lastName), text(userEmail), text(gender));
         $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text(firstName + " " + lastName));
-        $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
-        $x("//td[text()='Student Email']").parent().shouldHave(text(userEmail));
-        $x("//td[text()='Gender']").parent().shouldHave(text(gender));
         $x("//td[text()='Mobile']").parent().shouldHave(text(userNumber));
         $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
         $x("//td[text()='Subjects']").parent().shouldHave(text(subject1 + ", " + subject2));
